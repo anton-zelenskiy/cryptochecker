a226c8b1c5bfc088d5df33314f39c28b91295c92
Anton Zelenskiy <antonfewwt@gmail.com>
2019-06-02 17:17:47 +0700

    fix path

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import logging
 import telebot
 
 API_TOKEN = '825274529:AAFZVv3DYYmCvSBUl8HI3D8FbhiCNSudMvc'
-WEBHOOK_HOST = 'b55c6354.ngrok.io'  # 'zstoreit.info'
+WEBHOOK_HOST = 'zstoreit.info'
 WEBHOOK_PORT = 80
 
 WEBHOOK_URL_BASE = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}"
@@ -21,10 +21,9 @@ app = Flask(__name__)
 # Empty webserver index, return nothing, just http 200
 @app.route('/', methods=['GET', 'HEAD'])
 def index():
-    # print('tut')
-    # bot.remove_webhook()
-    # time.sleep(0.1)
-    # bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, max_connections=1)
+    bot.remove_webhook()
+    time.sleep(0.1)
+    bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, max_connections=1)
 
     return 'Hi there!'
 
@@ -32,7 +31,6 @@ def index():
 # Process webhook calls
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
-    print('>>>')
     if flask.request.headers.get('content-type') == 'application/json':
         json_string = flask.request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
