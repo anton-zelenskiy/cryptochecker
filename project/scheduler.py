da0f3121548452cbd42a4e5955132c067003ffcd
Anton Zelenskiy <antonfewwt@gmail.com>
2022-03-09 02:38:06 +0400

    fix

--- project/scheduler.py
+++ project/scheduler.py
@@ -5,7 +5,7 @@ from project.api.telegram import TelegramAPI
 from project.config import CHATS_CACHE_KEY, REDIS_HOST, REDIS_PORT
 
 cg = CoinGeckoAPI()
-redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
+redis = Redis(host=REDIS_HOST, port=REDIS_PORT, password='redis_password')
 tg_api = TelegramAPI()
 
 
