5396aab2e9aca757c45c7509c93aecdb7238bb70
Anton Zelenskiy <antonfewwt@gmail.com>
2024-12-10 14:07:14 +0700

    disable 2h check

--- project/celery_app.py
+++ project/celery_app.py
@@ -28,11 +28,11 @@ CELERY_CONFIG = {
             'schedule': crontab(hour='*/1', minute=1),
             'args': (60,)
         },
-        'task_check_volatility_120': {
-            'task': 'project.scheduler.tasks.task_check_volatility',
-            'schedule': crontab(hour='*/2', minute=1),
-            'args': (120,)
-        },
+        # 'task_check_volatility_120': {
+        #     'task': 'project.scheduler.tasks.task_check_volatility',
+        #     'schedule': crontab(hour='*/2', minute=1),
+        #     'args': (120,)
+        # },
         'task_send_currency_prices': {
             'task': 'project.scheduler.tasks.task_send_currency_prices',
             'schedule': crontab(hour='*/4', minute=0)
